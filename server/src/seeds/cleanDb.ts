27c1bdd00987a2f9622c8ee9f15669dce883d02f
Matt Mullen <mhmullen@icloud.com>
2025-05-09 08:23:27 -0700

    revised cleanDB

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,16 +1,39 @@
-import models from '../models/index.js';
-import db from '../config/connection.js';
+// import models from '../models/index.js';
+// import db from '../config/connection.js';
+
+// export default async (modelName: "Question", collectionName: string) => {
+//   try {
+//     let modelExists = await models[modelName].db.db.listCollections({
+//       name: collectionName
+//     }).toArray()
+
+//     if (modelExists.length) {
+//       await db.dropCollection(collectionName);
+//     }
+//   } catch (err) {
+//     throw err;
+//   }
+// }
+import models from "../models/index.js";
+import db from "../config/connection.js";
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
+    const listCollections = model?.db?.db?.listCollections;
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
+    if (listCollections) {
+      const modelExists = await listCollections
+        .call(model.db.db, {
+          name: collectionName,
+        })
+        .toArray();
+
+      if (modelExists.length) {
+        await db.dropCollection(collectionName);
+      }
     }
   } catch (err) {
     throw err;
   }
-}
+};
